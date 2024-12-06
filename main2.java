54d08c861ec322f2a0a510f3c9ef293942ae6c27
navateja5994 <navatejar80@gmail.com>
2024-12-06 09:30:37 +0530

    Add files via upload

---
+++ main2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main2
+{
+    public static void main(String args[])
+    {
+        int i=10;
+        int n=10;
+        while(i>=1)
+        {
+         System.out.println(n-1);
+         i-=1;   
+        }
+    }
+}
