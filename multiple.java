ec01263c4da6db9c65768d2ff0e3f4f696f08ef6
navateja5994 <navatejar80@gmail.com>
2024-12-06 09:30:51 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class multiple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<0;i++)
+        {
+            int n=sc.nextInt();
+            if((n&7)==0)
+            {
+             System.out.println("multiple of 8");  
+            }
+            else
+            {
+                System.out.println("not a multiple of 8");
+            }
+
+        }
+    }
+}
